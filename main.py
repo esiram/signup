7889b835d1ce42775710854b5b6dd19b0a921a7e
Esther Siram <isusecalea@yahoo.com>
2016-08-25 16:46:12 -0500

    WIP

--- main.py
+++ main.py
@@ -1,25 +1,103 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi
+
+form = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>SignUp Unit2 Homework2</title>
+</head>
+
+<body>
+  <form method="post">
+  <h1>Sign Up</h1>
+      <label><b>User Name:</b>
+        <input type="text" name="username" value="%(username)s"
+                           style="height: 20px; width: 200px">&nbsp;<div style="color: red">%(username_error)s</div>
+      </label>
+  <br>
+      <label><b>Password:</b>
+        <input type="password" name="password"
+                           style="height: 20px; width: 200px">&nbsp;<div style="color: red">%(password_error)s</div>
+     </label>
+  <br>
+      <label><b>Verify Password:</b>
+        <input type="password" name="password2"
+                           style="height: 20px; width: 200px">&nbsp;<div></div>
+      </label>
+  <br>
+      <label><b>Email (Optional):</b>
+      <input type="email" name="email" value="%(email)s"
+                           style="height: 20px; width: 200px">&nbsp;<div style="color: red">%(email_error)s</div>
+      </label>
+  <br>
+    <p><input type="submit" value="Submit"></p>
+</form>
+</body>
+<footer>
+</footer>
+</html>
+"""
+
+def escape_html(s):
+    return cgi.escape(s, quote = True)
+
+def valid_name(name):
+    if  not " " in name:
+        return True
+    else:
+        return False
+
+def valid_email(email):
+    if "@" in email and "." in email:
+        return True
+    else:
+        return False
+
+class MainSignUpHandler(webapp2.RequestHandler):
+    def write_form(self, username = "", email = "", username_error = "", password_error = "", email_error = ""):
+        self.response.out.write(form % {"username": username,
+                                        "email": email,
+                                        "username_error": username_error,
+                                        "password_error": password_error,
+                                        "email_error": email_error})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        user_name = self.request.get('username')
+        user_password = self.request.get('password')
+        user_password2 = self.request.get('password2')
+        user_email = self.request.get('email')
+
+        username = escape_html(user_name)
+        password = escape_html(user_password)
+        password2 = escape_html(user_password2)
+        email = escape_html(user_email)
+
+        username_error = ""
+        password_error = ""
+        email_error = ""
+
+        if not username or not valid_name(username):
+            username_error = "Invalid username."
+        if email and not valid_email(email):
+            email_error = "Invalid email address!"
+        if password and password2 and password != password2:
+            password_error = "Passwords don't match."
+
+        if username_error or password_error or email_error:
+            self.write_form(username, email, username_error, password_error, email_error)
+        else:
+            self.redirect('/welcome')
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainSignUpHandler),
+    ('/welcome', Welcome)
 ], debug=True)
